3bc1f21ab49872302a501b8bbc129379a14d8d8c
omid <omidomirzaei@gmail.com>
2023-01-04 11:31:50 +0330

    added text complition cubit

---
+++ lib/features/text_completion/presentation/cubit/text_completion_cubit.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:chatgpt_mobile/server/custom_exceptions.dart';
+import 'package:chatgpt_mobile/features/text_completion/data/model/text_completion_model.dart';
+import 'package:chatgpt_mobile/features/text_completion/domain/usecases/text_completion_usecase.dart';
+
+part 'text_completion_state.dart';
+
+class TextCompletionCubit extends Cubit<TextCompletionState> {
+  final TextCompletionUseCase textCompletionUseCase;
+  TextCompletionCubit({required this.textCompletionUseCase}) : super(TextCompletionInitial());
+
+  Future<void> textCompletion({required String query}) async {
+    emit(TextCompletionLoading());
+    try {
+      final textCompletionModelData = await textCompletionUseCase.call(query);
+      emit(TextCompletionLoaded(textCompletionModelData: textCompletionModelData));
+    } on SocketException catch (e) {
+      emit(TextCompletionFailure(errorMsg: e.message));
+    } on ServerException catch (e) {
+      emit(TextCompletionFailure(errorMsg: e.message));
+    }
+  }
+}
